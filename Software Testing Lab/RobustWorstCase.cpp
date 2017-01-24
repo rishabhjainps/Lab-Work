bda99e11da500f0bfb78c9d1340e771cf9ffbcfb
Rishabh <rishabhjainps@gmail.com>
2017-01-24 15:12:36 +0530

    Worst Added

--- Software Testing Lab/RobustWorstCase.cpp
+++ Software Testing Lab/RobustWorstCase.cpp
@@ -1,41 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-// 4n + 1
+// 5^n
 typedef long long ll;
 #define MOD 1000000007
 
 int n,a[10000][1000],minimum,maximum;
-int main()
-{
+int main() {
     cout<<"Enter the number of input variables (max.100) & [ minimum , maximum ] value  :";
     cin>>n;
     cin>>minimum>>maximum;
     int counter=-1;
-    int values[]={minimum,minimum+1,maximum-1,maximum};
+    int values[]={minimum-1,minimum,minimum+1,(minimum + maximum )/2,maximum-1,maximum,maximum+1};
     int m = sizeof(values)/sizeof(values[0]);
-    int optimal = (minimum + maximum )/2;
-    for(int i=0 ; i<n; i++){
-        for(int v=0; v<m; v++){
-            counter++;
-            for(int j=0 ; j<n; j++){
-                if(i==j)
-                    a[counter][j]=values[v];
-                else
-                    a[counter][j]=optimal;
-            }
+    int range = pow(7,n);
+    for(int i=0; i<range; i++){
+        int k=i;
+        for(int j=0; j<n; j++){
+            a[i][j] = values[k%m];
+            k=k/m;
         }
     }
-    counter++;
-    for(int j=0 ; j<n; j++){
-        a[counter][j]=optimal;
-    }
+    cout<"\n\n";
     cout<<setw(8)<<"TEST ID";
     for(int j=0 ; j<n; j++){
             cout<<setw(8)<<j+1;
     }
     cout<<"\n\n";
-    // 4n+1 : Boundary Values
-     for(int i=0 ; i<4*n+1; i++){
+    // 5^n : Worst case Values
+     for(int i=0 ; i<range; i++){
             cout<<setw(8)<<i+1;
             for(int j=0 ; j<n; j++){
                cout<<setw(8)<<a[i][j];
